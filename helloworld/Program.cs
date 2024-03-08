40d734284edf812a6b261ada4089e2223b57c7a1
unknown <sunnyjate2007@gmail.com>
2024-03-08 23:57:25 +0530

    ok

---
+++ helloworld/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Sunny!");
+
+string firstFriend = "       Maria     ";
+firstFriend = firstFriend.Trim();
+string secondFriend = "Sage    ";
+// secondFriend = secondFriend.TrimEnd();
+String friends = $"My friends are {firstFriend} and {secondFriend.Trim()}.";
+
+Console.WriteLine(friends);
+
+Console.WriteLine($"{friends.ToUpper()}, {friends.ToLower()}");
+//Console.WriteLine(friends.ToLower());
+Console.WriteLine(friends = friends.Replace("Sage","Samar"));
+
+Console.WriteLine(friends.Contains("Sa"));
+
+Console.WriteLine(friends.Length);
+
+Console.WriteLine(friends.StartsWith("M"));
+Console.WriteLine(friends.EndsWith(".   "));
